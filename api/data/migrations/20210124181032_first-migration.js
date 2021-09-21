172e8aeff7ef9eadee80e4a1aa886dfae5c82c92
Andrew Cummings <andrew.amalee@gmail.com>
2021-09-21 09:38:23 -0700

    tables finished

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -1,13 +1,37 @@
 exports.up = async (knex) => {
   await knex.schema
-    .createTable('users', (users) => {
-      users.increments('user_id')
-      users.string('username', 200).notNullable()
-      users.string('password', 200).notNullable()
-      users.timestamps(false, true)
+    .createTable("users", (table) => {
+      table.increments("user_id");
+      table.string("username", 200).notNullable();
+      table.string("password", 200).notNullable();
+      table.timestamps(false, true);
     })
-}
+    .createTable("owners", (table) => {
+      table.increments("owner_id");
+      table
+        .integer("user_id")
+        .notNullable()
+        .unsigned()
+        .references("user_id")
+        .inTable("users");
+    })
+    .createTable("items", (table) => {
+      table.increments("item_id");
+      table
+        .integer("owner_id")
+        .notNullable()
+        .unsigned()
+        .references("owner_id")
+        .inTable("owners");
+      table.integer("location").notNullable().unsigned();
+      table.string("name").notNullable();
+      table.string("description");
+      table.integer("price").notNullable().unsigned();
+    });
+};
 
 exports.down = async (knex) => {
-  await knex.schema.dropTableIfExists('users')
-}
+  await knex.schema.dropTableIfExists("items");
+  await knex.schema.dropTableIfExists("owners");
+  await knex.schema.dropTableIfExists("users");
+};
--- api/server.js
+++ api/server.js
@@ -24,7 +24,6 @@ server.use("/api/auth", restrict, authRouter);
 
 server.use((err, req, res, next) => {
   // eslint-disable-line
-
   res.status(err.status || 500).json({
     message: err.message,
     stack: err.stack,
